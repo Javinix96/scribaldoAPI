2d5730e503d1b3f348d36da59a1199d9d202e62e
javier <javier.rodriguez.9620@gmail.com>
2025-09-06 16:14:27 -0600

    Se cambia por hash

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 5999;
 app.use(express.json());
 
 app.get('/hola', (req, res) => {
-  console.log("obtner token",req.query.access_token);
+  console.log("obtner token",window.location.hash);
   res.redirect("scribaldo://scribaldogame");
 });
 
