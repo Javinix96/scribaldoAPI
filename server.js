d5471e883e5364c6dd01fba2d69ef6f7a4faa0f4
javier <javier.rodriguez.9620@gmail.com>
2025-09-05 22:12:50 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+
+const app = express();
+// const PORT = process.env.PORT || 3000;
+const PORT = 5999;
+
+app.use(express.json());
+
+app.get('/hola', (req, res) => {
+    console.log("fdsffddfsfsd");
+  res.redirect("scribe://scribaldo");
+});
+
+// 404
+app.use((req, res) => {
+  res.status(404).json({ error: 'Ruta no encontrada' });
+});
+
+// Manejador de errores
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: 'Error del servidor' });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
